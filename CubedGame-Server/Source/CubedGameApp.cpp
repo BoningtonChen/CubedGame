b28630ab9cd9d18243183c27e8b278d5f50a6be8
Cle2ment Chen <yikun.chen@163.com>
2025-02-15 22:47:00 +0800

    初始化设置Server的消息系统，使其能够正常工作，并且测试连接localhost的port8192

--- CubedGame-Server/Source/CubedGameApp.cpp
+++ CubedGame-Server/Source/CubedGameApp.cpp
@@ -1,9 +1,10 @@
+#include "ServerLayer.h"
 #include "Walnut/Application.h"
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Core/Log.h"
 
-#include <chrono>
+#include "ServerLayer.h"
 
 class ExampleLayer : public Walnut::Layer
 {
@@ -12,10 +13,7 @@ public:
 
 	virtual void OnUpdate(float ts) override
 	{
-		WL_INFO("Server Time steps: {}", ts);
-
-		using namespace std::chrono_literals;
-		std::this_thread::sleep_for(5ms);
+		// WL_INFO("Server Time steps: {}", ts);
 	}
 private:
 	bool m_AboutModalOpen = false;
@@ -27,8 +25,7 @@ Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 	spec.Name = "CubedGame Server";
 
 	Walnut::Application* app = new Walnut::Application(spec);
-	std::shared_ptr<ExampleLayer> exampleLayer = std::make_shared<ExampleLayer>();
-	app->PushLayer(exampleLayer);
+	app->PushLayer<CubedGame::ServerLayer>();
 	
 	return app;
 }
